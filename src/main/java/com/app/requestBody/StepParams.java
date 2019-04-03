8ab3c11292f5a77830b677bd54e06f1ad4992225
wx <wxdwlai@163.com>
2019-04-03 15:46:50 +0800

    first commit

---
+++ src/main/java/com/app/requestBody/StepParams.java
@@ -0,0 +1,17 @@
+package com.app.requestBody;
+
+import com.app.model.Steps;
+
+import java.util.List;
+
+public class StepParams {
+    private List<Steps> stepsList;
+
+    public List<Steps> getStepsList() {
+        return stepsList;
+    }
+
+    public void setStepsList(List<Steps> stepsList) {
+        this.stepsList = stepsList;
+    }
+}
